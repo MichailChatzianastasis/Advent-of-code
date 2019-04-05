52a2e535f090b7d8b9b0d1d3377e069d7ced7108
MichailChatzianastasis <tablakias@hotmail.gr>
2019-04-05 16:22:26 +0300

    high entropy

--- Day4/High-Entropy.py
+++ Day4/High-Entropy.py
@@ -5,3 +5,15 @@ Created on Fri Apr  5 16:04:20 2019
 @author: mixalis
 """
 
+f = open("Input.txt", "r")
+result=0
+for line in f:
+    valid=True
+    tokens=line.split()
+    freq = nltk.FreqDist(tokens)
+    for val in freq.values():
+        if(val>1):
+            valid=False
+    if(valid==True):
+        result+=1
+print(result)
